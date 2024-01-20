30ad7a1edd2c85a90fb23755efe8f109ef564069
Putt0 <reinaldymc4@gmail.com>
2024-01-20 20:46:21 -0300

    Music model in interface

---
+++ src/github/com/putt0/TableModel/MusicModel.java
@@ -0,0 +1,121 @@
+package github.com.putt0.TableModel;
+
+/**
+ * Author - Putt0;
+ */
+
+import github.com.putt0.entities.Music;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+public class MusicModel extends AbstractTableModel {
+
+    private final String[] colunas = { "Nome", "Autor", "Album" };
+    private final ArrayList<Music> linhas;
+
+    public MusicModel() {
+        linhas = new ArrayList<>();
+    }
+
+    public MusicModel(ArrayList<Music> linhas) {
+        this.linhas = linhas;
+    }
+
+
+    public int getRowCount() {
+        return linhas.size();
+    }
+
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Music music = linhas.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                return music.getName();
+            case 1:
+                return music.getAuthor();
+            case 2:
+                return music.getAlbum();
+        }
+        return null;
+    }
+
+
+    public void setValueAt(Object objetct, int rowIndex, int columnIndex) {
+        Music music = linhas.get(rowIndex);
+        String value = (String)objetct;
+        switch (columnIndex) {
+            case 0:
+                music.setName(value);
+                break;
+            case 1:
+                music.setAuthor(value);
+                break;
+            case 2:
+                music.setAlbum(value);
+                break;
+        }
+        fireTableRowsInserted(rowIndex, rowIndex);
+    }
+
+    public String getColumnName(int column) {
+        return colunas[column];
+    }
+
+    public Class<?> getColumnClass(int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                return String.class;
+            case 1:
+                return String.class;
+            case 2:
+                return String.class;
+        }
+        return null;
+    }
+
+
+    public void limpar() {
+        linhas.clear();
+        fireTableDataChanged();
+    }
+
+    public void addListaDeMusicas(List<Music> musicas) {
+        int indice = getRowCount();
+        linhas.addAll(musicas);
+
+        fireTableRowsInserted(indice, indice + musicas.size());
+    }
+
+    public Music getMusica(int rowIndex) {
+        return linhas.get(rowIndex);
+    }
+
+    public void addMusica(Music musica) {
+        linhas.add(musica);
+        int ultimoIndice = getRowCount() - 1;
+
+        fireTableRowsInserted(ultimoIndice, ultimoIndice);
+    }
+
+
+    public void removeMusica(int indiceLinha) {
+        linhas.remove(indiceLinha);
+        fireTableRowsDeleted(indiceLinha, indiceLinha);
+    }
+
+    public ArrayList<Music> getAsArrayList() {
+        return linhas;
+    }
+
+    public void mistura() {
+        Collections.shuffle(linhas);
+
+        fireTableRowsUpdated(0, linhas.size() - 1);
+    }
+}
