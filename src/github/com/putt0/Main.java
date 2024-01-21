21515fe2467806a2d47988ec5ea3dc701db1c6cf
Putt0 <reinaldymc4@gmail.com>
2024-01-21 13:35:12 -0300

    Upload class Main; General Project load

--- src/github/com/putt0/Main.java
+++ src/github/com/putt0/Main.java
@@ -4,6 +4,18 @@ package github.com.putt0;
  * Author - Putt0;
  */
 
+import github.com.putt0.formulate.FormulateMain;
+
+import java.io.IOException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 public class Main {
 
+    public Main() {}
+
+    public static void main(String[] args)
+            throws UnsupportedAudioFileException, IOException
+    {
+        new FormulateMain().setVisible(true);
+    }
 }
